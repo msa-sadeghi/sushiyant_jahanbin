ce35698ec9573343de46b3f0cb78361cce00302d
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-01-06 17:35:07 +0330

    s

---
+++ session1.py
@@ -0,0 +1,27 @@
+# print('hello world')
+# print("---------------")
+# print("welcome to python class")
+# print(1)
+# print(1.3)
+# print(1.3 + 3.5)
+
+# I want to define a variable
+# name = "artin"
+# print("hello", name)
+# print("How are you", name)
+
+# score = 10
+# print("your current score is:", score)
+# score = score + 5
+# print("your current score is:", score)
+# n1 = 10
+# number2 = 20
+# result = n1 + number2
+# print(result)
+
+
+# name = input("enter your name: ")
+# print("hello", name)
+
+z = "hi"
+print(z * 5)
